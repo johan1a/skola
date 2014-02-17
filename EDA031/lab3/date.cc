49ca12037ae3492e2fd6cf69bf3e402863546f53
Johan Andersson <johan.peter.andersson@gmail.com>
2014-02-17 13:25:42 +0000

    Måste prova på bärbara

--- EDA031/lab3/date.cc
+++ EDA031/lab3/date.cc
@@ -1,5 +1,7 @@
 #include <ctime>  // time and localtime
+#include "stdlib.h"
 #include "date.h"
+using namespace std;
 
 int Date::daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
@@ -11,20 +13,48 @@ Date::Date() {
 	day = locTime->tm_mday;
 }
 
-Date::Date(int y, int m, int d) {}
+Date::Date(int y, int m, int d) : year(y), month(m), day(d) {}
+
+istream& operator>>(istream& is, Date date){
+	string y,m,d;
+	getline(is,y,'-');
+	getline(is,m,'-');
+	getline(is,d);
+	date.year = atoi(y.c_str());
+	date.month = atoi(m.c_str());
+	date.day = atoi(d.c_str());
+	return is;
+}
+
+ostream& operator<<(ostream& os, const Date date){
+	os << date.getYear() << '-' << date.getMonth() << '-' << date.getDay();
+	return os;
+}
+
 
 int Date::getYear() const {
-	return 0;
+	return year;
 }
 
 int Date::getMonth() const {
-	return 0;
+	return month;
 }
 
 int Date::getDay() const {
-	return 0;
+	return day;
 }
 
 void Date::next() {
+	if(day == daysPerMonth[month-1]){
+		if(month == 12){
+			year++;
+			month = 1;
+		}else{
+			month++;
+		}
+		day = 1;
+	}else{
+		day++;
+	}
 }
 
--- EDA031/lab3/date.h
+++ EDA031/lab3/date.h
@@ -1,5 +1,7 @@
 #ifndef DATE_H
 #define DATE_H
+#include <iostream>
+
 
 class Date {
 public:
@@ -9,6 +11,8 @@ public:
 	int getMonth() const;      // get the month
 	int getDay() const;        // get the day
 	void next();               // advance to next day
+	friend std::istream& operator>>(std::istream& is, Date date);
+	friend std::ostream& operator>>(std::ostream& os, const Date date);
 private:
 	int year;  // the year (four digits)
 	int month; // the month (1-12)
